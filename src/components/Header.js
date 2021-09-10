313450095f9f8b71bd384a40e759ed3eaa862e8e
etoolabode <etoolabode@gmail.com>
2021-09-10 06:29:45 +0100

    done for now

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,13 +11,16 @@ export default function Header() {
       <div className="p-5 bg-red-500 fixed top-0 w-full z-50 ">
         <div className="flex justify-between sm:w-8/12 m-auto">
           <Link to="/">
-            <h1 className="text-gray-50 text-4xl text-center">Store</h1>
+            <div className="flex text-4xl text-yellow-200 font-semibold">
+              fast
+              <h1 className="text-gray-50  text-center">Store</h1>
+            </div>
           </Link>
           <Link to="/cart">
-            <div className="bg-yellow-500 rounded-full relative w-10 h-10 flex flex-col justify-center items-center">
+            <div className="bg-yellow-500 rounded-full relative top-2 w-8 h-8 flex flex-col justify-center items-center">
               <div
                 className="bg-gray-800 absolute rounded-full
-             text-gray-50 w-8 h-8 flex items-center justify-center bottom-5 left-5"
+             text-gray-50 w-6 h-6 flex items-center justify-center bottom-5 left-5"
               >
                 {cart.length}
               </div>
